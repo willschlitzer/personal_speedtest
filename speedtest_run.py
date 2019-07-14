4ff68a57da9c1c0e1594fa716f9144917e6d36bc
Will Schlitzer <schlitzer90@gmail.com>
2019-07-14 20:04:32 +0900

    Create old data folder, change run to only 1 thread and multi-thread, shorten waiting period

--- speedtest_run.py
+++ speedtest_run.py
@@ -4,18 +4,17 @@ import csv
 import datetime
 import time
 
+file = "speedtesting_all_threads.csv"
 
 def speedtest_func(threads, file):
     if not os.path.isfile(file):
-        headers = [['date_time_(JST)', 'download', 'upload']]
+        headers = [['date_time_(JST)', 'download', 'upload', 'threads']]
         with open(file, 'w') as writeFile:
             writer = csv.writer(writeFile)
             writer.writerows(headers)
 
     date_object = datetime.datetime.now()
-    current_date_string = date_object.strftime("%d/%m/%y %H:%M")
-    date_string = date_object.strftime("%d/%m/%y")
-    time_string = date_object.strftime("%H:%M")
+    current_date_string = date_object.strftime("%d/%m/%y %H:%M:%S")
     servers = []
 
     s = speedtest.Speedtest()
@@ -29,7 +28,10 @@ def speedtest_func(threads, file):
 
     download = int(results_dict['download'])
     upload = int(results_dict['upload'])
-    new_row = [current_date_string, download, upload]
+    if threads == None:
+        new_row = [current_date_string, download, upload, 'None']
+    else:
+        new_row = [current_date_string, download, upload, str(threads)]
 
     with open(file, 'a') as csvFile:
         writer = csv.writer(csvFile)
@@ -39,9 +41,9 @@ def speedtest_func(threads, file):
 running = True
 
 while running:
-    speedtest_func(threads=1, file = "speedtesting_single_thread.csv")
-    speedtest_func(threads=None, file="speedtesting_multi_thread.csv")
+    speedtest_func(threads=1, file = file)
+    speedtest_func(threads=None, file=file)
     date_object = datetime.datetime.now()
     current_date_string = date_object.strftime("%d/%m/%y %H:%M")
     print(current_date_string)
-    time.sleep(300)
+    time.sleep(120)
