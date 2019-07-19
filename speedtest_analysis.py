06e90f6f529d6322f051604e5a744e2b4f117195
Will Schlitzer <schlitzer90@gmail.com>
2019-07-19 18:35:04 +0900

    update hourly filter

--- speedtest_analysis.py
+++ speedtest_analysis.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-file = "speedtesting_single_thread.csv"
+file = "speedtesting_all_threads.csv"
 
 df = pd.read_csv(file, index_col="date_time_(JST)", parse_dates=True)
+df_single = df[df['threads']=='1']
+df_multi = df[df['threads']=='None']
 resampled_df = df.resample("H").mean()
-print(resampled_df)
+#print(resampled_df)
+df_single_rhourly = df_single.resample("H").mean().dropna()
+#print(df_single_rhourly)
+df_single_rhourly.plot()
+plt.show()
