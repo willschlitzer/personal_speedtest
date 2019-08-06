dc8e8b971227f93b4d0b5758f4c4c3900aa4f513
Will Schlitzer <schlitzer90@gmail.com>
2019-08-06 20:23:44 +0900

    Add number logging for day of week

--- speedtest_analysis.py
+++ speedtest_analysis.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 file = "data_for_analysis/speedtesting_all_threads_19Jul19.csv"
 
 df = pd.read_csv(file, index_col="date_time_(JST)", parse_dates=True)
 resampled_df = df.resample("H").mean().dropna()
-print(resampled_df)
+#print(resampled_df)
+resampled_df.plot()
+plt.show()
