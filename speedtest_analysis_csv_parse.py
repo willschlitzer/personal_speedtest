571d6612dfdb3f0faa1be5111e6a72ae206f2a75
Will Schlitzer <schlitzer90@gmail.com>
2020-03-27 16:31:21 +0900

    Remove splitting functions, complete reading function

--- speedtest_analysis_csv_parse.py
+++ speedtest_analysis_csv_parse.py
@@ -1,30 +1,39 @@
 import csv
 from collections import namedtuple
+from datetime import datetime
 
 data_file = "testing_data/raw_data_testing_sample.csv"
-single_thread_file = "testing_data/single_thread_testing_sample.csv"
-multi_thread_file = "testing_data/multi_thread_testing_sample.csv"
 
-Data = namedtuple
+
+Data = namedtuple("Data", "date_string date_object day_num download upload threads")
+
+
 def get_speedtest_data(data=data_file):
+    data_list = []
     with open(data, encoding="utf-8") as f:
-        for line in csv.reader(f):
-            print(line)
-
-def split_data_by_thread(data=data_file, multi=multi_thread_file, single=single_thread_file):
-    with open(data, encoding="utf-8") as f:
-        for line in csv.reader(f):
-            if line[5] == '1':
-                write_to_csv(file=single, line=line)
-            else:
-                write_to_csv(file=multi, line=line)
-
-def write_to_csv(file, line):
-    with open(file, encoding="utf-8") as f:
-        write_line = csv.writer(f)
-        write_line.writerow(line)
+        for line in csv.DictReader(f):
+            try:
+                date_string = line["date_time_(JST)"]
+                date_object = datetime.strptime(date_string, "%d/%m/%y %H:%M:%S")
+                day_num = line["day_num"]
+                download = line["download"]
+                upload = line["upload"]
+                threads = line["threads"]
+            except ValueError:
+                continue
+            d = Data(
+                date_string=date_string,
+                date_object=date_object,
+                day_num=day_num,
+                download=download,
+                upload=upload,
+                threads=threads,
+            )
+            data_list.append(d)
+    for entry in data_list:
+        print(entry.threads)
 
 
 if __name__ == "__main__":
-    #get_speedtest_data()
-    #split_data_by_thread()
+    get_speedtest_data()
+    # split_data_by_thread()
